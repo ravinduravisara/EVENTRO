57808d3f1e5746934ab52d106fcf62774db2b851
Ravindu Ravisara <ravinduravisara@gmail.com>
2026-04-01 23:30:14 +0530

    Revert "Update app.js"
    This reverts commit 0b4c8c2600aa61f69f927427b0db9a612f19a993.

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,21 +16,6 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(passport.initialize());
 
-app.get('/', (_req, res) => {
-	res.status(200).json({
-		status: 'ok',
-		message: 'EVENTRO backend is running',
-		apiBase: '/api',
-	});
-});
-
-app.get('/api', (_req, res) => {
-	res.status(200).json({
-		status: 'ok',
-		message: 'EVENTRO API is running',
-	});
-});
-
 app.use('/api', routes);
 
 app.use(errorHandler);
